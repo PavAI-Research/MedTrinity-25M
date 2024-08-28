1c5265fb8d4eced3ba71b9c4ad0d2a0bf6fd4abe
minyang <mychen76@gmail.com>
2024-08-28 05:14:55 -0400

    Add files via upload

---
+++ test_cli.sh
@@ -0,0 +1,13 @@
+export PYTHONPATH=.
+
+# load the original llama 3.1 tokenizer using an active read-only hf_token
+export HF_TOKEN=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
+# tokenizer_model_path
+#export TOKENIZER_PATH=meta-llama/Meta-Llama-3.1-8B-Instruct
+export TOKENIZER_PATH=/home/pop/hosting/medical/med-model/LLaVA-Med-Captioner/llava-llama-med-8b-captioner
+
+python -m llava.serve.cli \
+    --model-path /home/pop/hosting/medical/med-model/LLaVA-Med-Captioner/llava-llama-med-8b-captioner \
+    --image-file "https://prod-images-static.radiopaedia.org/images/566180/d527ff6fc1482161c9225345c4ab42_big_gallery.jpg" 
+#    --load-4bit
+
